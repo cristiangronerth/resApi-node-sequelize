8eb37371c9b6cee53e23734dfaedc320e505dab7
cristiangronerth <cristiangronerth@gmail.com>
2023-07-09 23:57:00 -0300

    rutas de create y get

---
+++ src/routes/projects.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+const router = Router()
+import { getProjects, createProjects } from "../controllers/projects.controllers.js";
+
+
+
+router.get("/projects", getProjects)
+router.post("/projects", createProjects)
+router.put("/projects/:id")
+router.delete("/projects/:id")
+router.get("/projects/:id")
+
+
+
+
+export default router;
+
