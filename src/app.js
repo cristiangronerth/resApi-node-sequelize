68c4370b6d2f3d9c085d5c327a5e01fb1e089470
cristiangronerth <cristiangronerth@gmail.com>
2023-07-12 10:03:27 -0300

    rutas tasks create y read funcionando bien

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
 import express from "express";
 const app = express();
 import projectsRoutes from "./routes/projects.routes.js";
+import tasksRoutes from "./routes/tasks.routes.js";
+
 
 // middleware
 app.use(express.json())
@@ -9,5 +11,8 @@ app.use(express.json())
 
 
 app.use(projectsRoutes)
+app.use(tasksRoutes)
+
+
 
 export default app;
